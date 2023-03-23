86a27a7b72ea585c1ed377e3b00f8d6644384107
teo-garcia <juanmateogarcia96@gmail.com>
2023-03-23 14:42:23 -0500

    feat: #0 - bump dependencies

--- src/lib/types/client.ts
+++ src/lib/types/client.ts
@@ -3,16 +3,4 @@ export type ProvidersProps = {
   children: React.ReactNode
 }
 
-export type LayoutProps = {
-  children: React.ReactNode
-}
-
-export type SeoProps = {
-  description?: string
-  ogDescription?: string
-  ogImage?: string
-  ogTitle?: string
-  title: string
-}
-
 export type ThemeMode = 'light' | 'dark'
