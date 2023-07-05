c846232192bfe97a7417c7cb5481b8d9e1784af5
teo-garcia <juanmateogarcia96@gmail.com>
2023-07-05 17:32:01 -0500

    chore: bump dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'next/core-web-vitals',
     'plugin:jsx-a11y/recommended',
+    "plugin:tailwindcss/recommended",
     'plugin:jest/recommended',
     'plugin:testing-library/react',
     'plugin:playwright/playwright-test',
--- src/components/Providers/Providers.tsx
+++ src/components/Providers/Providers.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import type { ProvidersProps } from '@lib/types/client'
 
-/* React Query */
 const queryClient = new QueryClient()
 
 const Providers = (props: ProvidersProps) => {
