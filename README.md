34ee2e8472c5e52f9417522eb7c37aa6de892af1
Mateo Garcia <juanmateogarcia96@gmail.com>
2024-01-02 21:01:25 -0500

    chore(dependencies): bump dependencies manually

--- README.md
+++ README.md
@@ -35,15 +35,15 @@ Start building modern web applications using React and Next.js.
 
 ## Tools
 
-- Next 13.
+- Next 14.
 - React 18.
 - Tailwind 3.
 - Typescript 4.
 - ESLint 8 + Prettier 3.
-- Husky 8 + Lint Staged 13.
+- Husky 8 + Lint Staged 15.
 - Jest 29 + Testing Library React 14.
 - Playwright 1.
-- MSW 1.
+- MSW 2.
 
 ## Commands
 
--- src/features/NotFoundBanner/NotFoundBanner.tsx
+++ src/features/NotFoundBanner/NotFoundBanner.tsx
@@ -3,8 +3,8 @@ import { FaInfoCircle } from 'react-icons/fa'
 const NotFoundBanner = () => {
   return (
     <section className="flex h-screen flex-col items-center justify-center gap-y-12 bg-primary dark:bg-black">
-      <FaInfoCircle className="h-32 w-32 text-black dark:text-primary lg:h-36 lg:w-36" />
-      <h1 className="text-5xl font-semibold text-black dark:text-primary lg:text-7xl">
+      <FaInfoCircle className="h-32 w-32 text-black lg:h-36 lg:w-36 dark:text-primary" />
+      <h1 className="text-5xl font-semibold text-black lg:text-7xl dark:text-primary">
         Page not found
       </h1>
     </section>
