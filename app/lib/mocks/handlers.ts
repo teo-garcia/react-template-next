b876710e4d1989966bb17e6f2760e48bf6a1e347
Mateo Garcia <juanmateogarcia96@gmail.com>
2025-05-28 16:05:59 -0500

    fix(msw): update healthcheck endpoint

--- app/lib/mocks/handlers.ts
+++ app/lib/mocks/handlers.ts
@@ -1,14 +1,17 @@
 import { HttpResponse, http } from 'msw'
 
-const healthcheckHandler = http.get('http://localhost:3000/healthcheck', () => {
-  return HttpResponse.json(
-    {
-      message: 'ok',
-      status: 200,
-      data: null,
-    },
-    { status: 200 }
-  )
-})
+const healthcheckHandler = http.get(
+  'http://localhost:3000/api/healthcheck',
+  () => {
+    return HttpResponse.json(
+      {
+        message: 'ok',
+        status: 200,
+        data: null,
+      },
+      { status: 200 }
+    )
+  }
+)
 
 export const handlers = [healthcheckHandler]
