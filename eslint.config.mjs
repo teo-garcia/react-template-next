a9942b29b07d0f28b51f7d0e683c075566eb239a
teo-garcia <juanmateogarcia96@gmail.com>
2026-04-01 15:38:19 -0500

    chore: preserve eslint config import order

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+// eslint-disable-next-line simple-import-sort/imports
 import nextPlugin from '@next/eslint-plugin-next'
 import base from '@teo-garcia/eslint-config-shared/base'
 import { defineConfig } from 'eslint/config'
