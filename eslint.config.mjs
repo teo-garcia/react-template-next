eda0aa3e3de16d2099c8d24f0515816376b91229
teo-garcia <juanmateogarcia96@gmail.com>
2026-04-01 15:33:02 -0500

    refactor: adopt defineConfig syntax

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,12 @@
 import nextPlugin from '@next/eslint-plugin-next'
 import base from '@teo-garcia/eslint-config-shared/base'
+import { defineConfig } from 'eslint/config'
 import playwright from '@teo-garcia/eslint-config-shared/playwright'
 import react from '@teo-garcia/eslint-config-shared/react'
 
-export default [
+export default defineConfig([
   ...base,
   ...react,
   nextPlugin.default.configs.recommended,
   ...playwright,
-]
+])
